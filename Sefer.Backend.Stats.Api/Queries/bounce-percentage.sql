711a70e20f9e45dbe1ef78631fcc10364fe25111
martin-hillford <171671237+martin-hillford@users.noreply.github.com>
2025-08-19 19:34:04 +0300

    Fixed a division by zero error when the database is empty

--- Sefer.Backend.Stats.Api/Queries/bounce-percentage.sql
+++ Sefer.Backend.Stats.Api/Queries/bounce-percentage.sql
@@ -1,21 +1,21 @@
-SELECT s.c / (m.c + s.c) * 100 AS BouncePercentage
+SELECT s.c / GREATEST(m.c + s.c, 1) * 100 AS BouncePercentage
 FROM (
          SELECT CAST(COUNT(m.browser_token) AS DOUBLE PRECISION) AS c
          FROM (
-                  SELECT browser_token
-                  FROM client_page_request_log_entries
-                  WHERE browser_token IS NOT NULL $1
-                  GROUP BY browser_token
-                  HAVING COUNT("browser_token") > 1
+              SELECT browser_token
+              FROM client_page_request_log_entries
+              WHERE browser_token IS NOT NULL $1
+              GROUP BY browser_token
+              HAVING COUNT("browser_token") > 1
          ) AS m
      ) AS m,
      (
          SELECT CAST(COUNT(s.browser_token) AS DOUBLE PRECISION) AS c
          FROM (
-                  SELECT browser_token
-                  FROM client_page_request_log_entries
-                  WHERE browser_token IS NOT NULL $1
-                  GROUP BY browser_token
-                  HAVING COUNT("browser_token") = 1
-              ) AS s
+              SELECT browser_token
+              FROM client_page_request_log_entries
+              WHERE browser_token IS NOT NULL $1
+              GROUP BY browser_token
+              HAVING COUNT("browser_token") = 1
+          ) AS s
      ) AS s
